1b474dda8da6c41f8359f93130e9436c587b8027
nirvik saha <nsdev404@gmail.com>
2024-09-09 12:51:52 -0400

    starting google sheets work;

---
+++ google_sheets.py
@@ -0,0 +1,37 @@
+import os.path
+import gspread
+from google.oauth2.service_account import Credentials
+
+def get_last_row(ns_worksheet):
+    row_count=0
+    while True and row_count<10:
+        row_count +=1 
+        try:
+            v = ns_worksheet.row_values(row_count)[0]
+            if v=='':
+                break
+        except:
+            break
+    return row_count
+
+def update_worksheet_entry(ns_worksheet, user_question="-", bot_answer="x"):
+    row_count = get_last_row(ns_worksheet)
+    ns_worksheet.update_cell(row_count, 1, str(row_count))
+    ns_worksheet.update_cell(row_count, 2, user_question)
+    ns_worksheet.update_cell(row_count, 3, bot_answer)
+
+def initialize_gspread():
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds  = Credentials.from_service_account_file("credentials.json", scopes = scopes)
+    client = gspread.authorize(creds)
+
+    sheet_id = "1XK9lPuT4rjIBrymeCn5ikk3TBSEMaqBarXfQn5SiTSA"
+    workbook = client.open_by_key(sheet_id)
+    worksheet = workbook.get_worksheet(0)
+
+    user_question="hello"
+    bot_answer = "world"
+    update_worksheet_entry(worksheet, user_question, bot_answer)
+
+if __name__ == "__main__":
+    initialize_gspread()
--- main.py
+++ main.py
@@ -9,8 +9,30 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 
 from dotenv import load_dotenv
-
 load_dotenv()
+
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+def get_last_row(ns_worksheet):
+    row_count=0
+    while True and row_count<10:
+        row_count +=1 
+        try:
+            v = ns_worksheet.row_values(row_count)[0]
+            if v=='':
+                break
+        except:
+            break
+    return row_count
+
+def update_worksheet_entry(ns_worksheet, user_question="-", bot_answer="x"):
+    row_count = get_last_row(ns_worksheet)
+    ns_worksheet.update_cell(row_count, 1, str(row_count))
+    ns_worksheet.update_cell(row_count, 2, user_question)
+    ns_worksheet.update_cell(row_count, 3, bot_answer)
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -65,7 +87,6 @@ def handle_userinput(user_question):
 def main():
     st.set_page_config(page_title="Perkins&Will", page_icon=":sparkles:")
     st.write(css, unsafe_allow_html=True)
-
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
@@ -88,4 +109,12 @@ def main():
 
 
 if __name__ == '__main__':
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds  = Credentials.from_service_account_file("credentials.json", scopes = scopes)
+    client = gspread.authorize(creds)
+
+    sheet_id = "1XK9lPuT4rjIBrymeCn5ikk3TBSEMaqBarXfQn5SiTSA"
+    workbook = client.open_by_key(sheet_id)
+    worksheet = workbook.get_worksheet(0)
+
     main()
